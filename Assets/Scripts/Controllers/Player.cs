0f6aeccda44e3fd935d84d5f59f4a0b109cd064e
LeonGu0402 <2653579853@qq.com>
2024-09-17 14:10:50 -0400

    Task 1a
    update the playerMovement function

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -9,9 +9,39 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    public float velocity = 0.3f;
+
     void Update()
     {
-
+        
     }
 
+    private void FixedUpdate()
+    {
+        playerMovement();
+    }
+
+
+    public void playerMovement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + velocity, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - velocity, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + velocity, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - velocity, transform.position.y, transform.position.z);
+        }
+    }
 }
