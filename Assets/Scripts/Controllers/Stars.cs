eefca09b3111552dff7104ce41e748a60b3823e1
LeonGu0402 <2653579853@qq.com>
2024-09-22 19:55:34 -0400

    Task 4
    Hard, hard and hard

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -7,8 +7,34 @@ public class Stars : MonoBehaviour
     public List<Transform> starTransforms;
     public float drawingTime;
 
+    private bool draw = true;
+
     // Update is called once per frame
     void Update()
     {
+
+        foreach (Transform t in starTransforms)
+        {
+            Vector3 position = t.position;
+            Vector3 nextposition = starTransforms[starTransforms.IndexOf(t) + 1].position;
+
+            if (draw)
+            {
+                drawLine(position, nextposition, drawingTime);
+            }
+
+        }
+
+
+    }
+
+    private void drawLine(Vector3 start, Vector3 end, float time)
+    {
+        draw = false;
+
+        Debug.DrawLine(start, end, Color.white, time);
+
+        draw = true;
+        
     }
 }
