38cde630f80d31c77f808319d6de614d3de74b25
LeonGu0402 <2653579853@qq.com>
2024-09-27 19:54:44 -0400

    week 4 task 3
    Done!

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
+    public float Radius;
+    public float Speed;
 
+    private float radian = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(Radius, Speed, planetTransform);
+    }
 
+
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+
+        radian += Time.deltaTime;
+
+        Vector3 direction = target.position + (new Vector3(Mathf.Cos(radian), Mathf.Sin(radian), 0).normalized) * radius - transform.position;
+        transform.position += direction * speed * Time.deltaTime;
+        
     }
 }
