11f7fce343c2ac5f24d6f00377311303a895b237
LeonGu0402 <2653579853@qq.com>
2024-10-14 17:02:14 -0400

    2nd mechanic
    enemy points towards player

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using static UnityEngine.GraphicsBuffer;
 
 public class Enemy : MonoBehaviour
 {
     public float speed;
 
     public Transform playerTransform;
+    public float rotationSpeed;
 
     private Vector3 enemyPosition;
     private Vector3 playerPosition;
@@ -22,8 +24,7 @@ public class Enemy : MonoBehaviour
         playerPosition = playerTransform.position;
 
         trackPlayer(enemyPosition, playerPosition, speed);
-
-
+        rotationEnemy(playerTransform, rotationSpeed);
     }
 
     private void trackPlayer(Vector3 enemy, Vector3 player, float speed)
@@ -32,6 +33,33 @@ public class Enemy : MonoBehaviour
         Vector3 velocity = direction.normalized * speed * Time.deltaTime;
 
         transform.position += velocity;
+
+    }
+
+    public void rotationEnemy(Transform player, float speed)
+    {
+        //Debug.DrawLine(transform.position, player.position, Color.green);
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //Debug.Log(angle);
+        //float facing = transform.rotation.eulerAngles.z + 90;
+
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+        ////if (angle - facing <= 0)
+        ////{
+        ////    if (facing > angle)
+        ////    {
+        ////        transform.Rotate(0, 0, -speed * Time.deltaTime);
+        ////    }
+        ////}
+        ////else
+        ////{
+        ////    if (facing < angle)
+        ////    {
+        ////        transform.Rotate(0, 0, speed * Time.deltaTime);
+        ////    }
+        ////}
+
 
     }
 
